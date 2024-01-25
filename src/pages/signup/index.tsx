f65db0230ce947a1e579907d7eee8bc4bef1a78d
dkile <dkile532@naver.com>
2024-01-25 08:57:03 +0000

    feat: 로고 클릭 시 공고 리스트 화면으로 이동

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,11 +1,26 @@
+import Image from "next/image";
+import Link from "next/link";
+
 import SignupForm from "@/components/signup/SignupForm";
 import DialogProvider from "@/providers/DialogProvider";
+import { PAGE_ROUTES } from "@/routes";
 
 export default function Page() {
   return (
     <DialogProvider>
       <section>
+        <Link href={PAGE_ROUTES.NOTICES}>
+          <Image
+            src="/icons/logo.svg"
+            alt="로고 이미지"
+            width={248}
+            height={48}
+          />
+        </Link>
         <SignupForm />
+        <div>
+          이미 가입하셨나요? <Link href={PAGE_ROUTES.SIGNIN}>로그인하기</Link>
+        </div>
       </section>
     </DialogProvider>
   );
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 export const PAGE_ROUTES = {
   SIGNUP: "/signup",
   SIGNIN: "/signin",
+  NOTICES: "/notices",
 };
 
 export const API_ROUTE = process.env.NEXT_PUBLIC_API_ENDPOINT;
