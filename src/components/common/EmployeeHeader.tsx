67f46d8013e5f10638881b5c21d358549dfcc04f
dkile <dkile532@naver.com>
2024-02-03 07:16:33 +0000

    design: 검색바 input 길이 수정

--- src/components/common/EmployeeHeader.tsx
+++ src/components/common/EmployeeHeader.tsx
@@ -1,13 +1,18 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useContext } from "react";
 
 import SearchBar from "@/components/common/SearchBar";
 import { Button } from "@/components/ui/button";
+import { UserActionContext, UserContext } from "@/providers/UserProvider";
 import { PAGE_ROUTES } from "@/routes";
 
 export default function EmployeeHeader() {
+  const user = useContext(UserContext);
+  const { logout } = useContext(UserActionContext);
+
   return (
-    <header className="m-auto flex max-w-[1088px] flex-wrap items-center justify-between gap-x-[32px] px-[20px]">
+    <header className="m-auto flex max-w-[1088px] flex-wrap items-center justify-between gap-x-[32px] px-[20px] py-[12px]">
       <Link href={PAGE_ROUTES.NOTICES} className="order-1 min-w-max p-[12px]">
         <Image
           src="/icons/logo.svg"
@@ -22,19 +27,41 @@ export default function EmployeeHeader() {
         <SearchBar />
       </div>
       <div className="order-2 flex items-center gap-[8px] text-[1.4rem] font-bold">
-        <Button
-          variant="ghost"
-          asChild
-          className="h-max rounded-[8px] p-[12px] text-[1.4rem] font-bold text-gray-600"
-        >
-          <Link href={PAGE_ROUTES.PROFILE}>내 프로필</Link>
-        </Button>
-        <Button
-          variant="ghost"
-          className="h-max rounded-[8px] px-[12px] py-[12px] text-[1.4rem] font-bold text-gray-600"
-        >
-          로그아웃
-        </Button>
+        {user ? (
+          <>
+            <Button
+              variant="ghost"
+              asChild
+              className="h-max rounded-[8px] p-[12px] text-[1.4rem] font-bold text-gray-600"
+            >
+              <Link href={PAGE_ROUTES.PROFILE}>내 프로필</Link>
+            </Button>
+            <Button
+              variant="ghost"
+              className="h-max rounded-[8px] px-[12px] py-[12px] text-[1.4rem] font-bold text-gray-600"
+              onClick={() => logout()}
+            >
+              로그아웃
+            </Button>
+          </>
+        ) : (
+          <>
+            <Button
+              asChild
+              variant="ghost"
+              className="h-max rounded-[8px] p-[12px] text-[1.4rem] font-bold text-gray-600"
+            >
+              <Link href={PAGE_ROUTES.SIGNIN}>로그인</Link>
+            </Button>
+            <Button
+              asChild
+              variant="ghost"
+              className="h-max rounded-[8px] p-[12px] text-[1.4rem] font-bold text-gray-600"
+            >
+              <Link href={PAGE_ROUTES.SIGNUP}>회원가입</Link>
+            </Button>
+          </>
+        )}
       </div>
     </header>
   );
--- src/components/common/SearchBar.tsx
+++ src/components/common/SearchBar.tsx
@@ -20,7 +20,7 @@ export default function SearchBar() {
     <div className="flex max-w-[480px] gap-[8px] rounded-[8px] bg-gray-10 p-[12px]">
       <Image src="/icons/search.svg" alt="" width={20} height={20} />
       <input
-        className="overflow-hidden bg-inherit text-[1.4rem] placeholder:text-[1.4rem] focus:outline-none"
+        className="w-full overflow-hidden bg-inherit text-[1.4rem] placeholder:text-[1.4rem] focus:outline-none"
         placeholder="가게 이름으로 찾아보세요"
         onKeyDown={handleEnterPressed}
       />
